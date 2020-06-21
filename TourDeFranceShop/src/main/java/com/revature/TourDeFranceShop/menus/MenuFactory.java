eefa71736eb8499f30cdc51780a695ed901fcb94
Reuben Hernandez <reuben1098@gmail.com>
2020-06-20 20:59:31 -0400

    login feature added to MenuFactory

--- TourDeFranceShop/src/main/java/com/revature/TourDeFranceShop/menus/MenuFactory.java
+++ TourDeFranceShop/src/main/java/com/revature/TourDeFranceShop/menus/MenuFactory.java
@@ -1,9 +1,13 @@
 package com.revature.TourDeFranceShop.menus;
 
 import com.revature.TourDeFranceShop.models.User;
+import com.revature.TourDeFranceShop.service.LoginService;
 
+//Class structure from Marielle Nolasco's github code
 public class MenuFactory {
-	public IMenu getMenu(User user) {
+	public IMenu getMenu() {
+		LoginService loginService = new LoginService();
+		User user = loginService.login();
 		switch(user.getRole()) {
 			case"customer":
 				return new CustomerMenu(user);
